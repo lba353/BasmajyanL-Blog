258fda00606a119b027afe67755319fe54074542
Ludvig Basmajyan <lbasmajyan@gmail.com>
2015-01-30 09:22:19 -0800

    Blog (End of 1/30)
    Added a bunch of files, fixed my paths, and making my blog better.

--- Controller/Create-DB.php
+++ Controller/Create-DB.php
@@ -4,7 +4,7 @@
     $connection = new mysqli($host, $username, $password);
     
     if($connection->connect_error) {
-        die("Error: " . $connection->connect_error);
+        die("<p>Error: " . $connection->connect_error . "</p>");
     }
     
     $exists = $connection->select_db($database);
@@ -13,17 +13,24 @@
         $query = $connection->query("CREATE DATABASE $database");
         
         if($query) {
-            echo "Successfully created a database: " . $database;
+            echo "<p>Successfully created a database: " . $database . "</p>";
         }
     }
     else {
-        echo "Database already exists.";
+        echo "<p>Database already exists.</p>";
     }
     
     $query = $connection->query("CREATE TABLE posts ("
             . "id int(11) NOT NULL AUTO_INCREMENT," 
-            . "title varchar255 NOT NULL,"
+            . "title varchar(255) NOT NULL,"
             . "post text NOT NULL,"
             . "PRIMARY KEY (id))");
+    
+    if($query) {
+        echo "<p>Successfully created table: posts</p>";
+    }
+    else {
+        echo "<p>$connection->error</p>";
+    }
     
     $connection->close();
---
+++ Model/Config.php
@@ -0,0 +1,2 @@
+<?php
+    $path = "/BasmajyanL-Blog/";
