b8e5d77013ef79b918d26003d13ab41c2e34ec7b
Ludvig Basmajyan <lbasmajyan@gmail.com>
2015-02-02 10:47:20 -0800

    Blog (End of 2/2)
    Made my first successful blog post along with a couple of files.

--- Post.php
+++ Post.php
@@ -1,8 +1,4 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    require_once(__DIR__ . "/View/Header.php");
+    require_once(__DIR__ . "/View/Form.php");
+    require_once(__DIR__ . "/View/Footer.php");
