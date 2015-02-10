bfeda71bbb104563a264d222658f1546c8f99ce1
Ludvig Basmajyan <lbasmajyan@gmail.com>
2015-02-10 09:19:51 -0800

    Blog (End of 2/10)
    Made a user register form and showed my posts.

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
+    //Requires the Header, Navigation, Create-DB, Footer, and Read Posts files once.
     require_once (__DIR__ . "/View/Header.php");
     require_once (__DIR__ . "/View/Navigation.php");
     require_once (__DIR__ . "/Controller/Create-DB.php");
     require_once (__DIR__ . "/View/Footer.php");
+    require_once (__DIR__ . "/Controller/Read-Posts.php");
